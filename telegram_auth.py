77d9a8811355166cb94b4c4d7d7ac90a8d11471a
kopyl <oleggggg1997@gmail.com>
2020-11-05 12:41:34 +0200

    Add files via upload

---
+++ telegram_auth.py
@@ -0,0 +1,6 @@
+
+"""
+Want to send logs messages to Telegram? Enter the bot token into parentheses below: ('')
+"""
+BOT_TOKEN = '359202515:AAGo85tMjNl-_muGxB3tDBiuQMed8JOdo0s'
+YOUR_TELEGRAM_ID = 35920251
